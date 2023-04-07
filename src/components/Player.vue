48e47a24015ea4c5980f46dfb4c0b72e17a11f85
michal <mguzy95@gmail.com>
2023-04-07 18:05:00 +0200

    Revert "a"
    This reverts commit e47ff8e20db6b17173369231a05c3f34cdab59c1.

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -117,7 +117,6 @@ export default {
         this.paused = true;
       });
       socket.on("room", (data) => {
-        console.log(data);
         this.$store.commit("setStatePlaylist", data.playlist);
         this.$store.commit("setIndex", data.currentIndex);
         // this.$store.commit("setCurrentVideo", data.currentVideo);
