8e8379e22730e0a7d3d27d38383f65704a1321f3
michal <mguzy95@gmail.com>
2023-04-07 18:05:31 +0200

    Revert "o"
    This reverts commit ba939651b890a3b70d722610c67546525c605a5b.

--- src/components/Library.vue
+++ src/components/Library.vue
@@ -171,7 +171,7 @@ export default {
           method: "POST",
           mode: "cors",
           headers: {
-            Accept: "application/json, text/plain, */*",
+            // Accept: "application/json",
             "Content-Type": "application/json",
             authorization: `Bearer ${this.user.token}`,
           },
@@ -190,7 +190,7 @@ export default {
           method: "GET",
           // mode: "cors",
           headers: {
-            Accept: "application/json, text/plain, */*",
+            // Accept: "application/json",
             // "Content-Type": "application/json",
             authorization: `Bearer ${this.user.token}`,
           },
