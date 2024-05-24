f4ec6f815a199fbe87d8dc9a6d14df507ba7a872
unknown <mguzy95@gmail.com>
2024-05-25 00:30:14 +0200

    socket secure

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,9 @@
 import io from "socket.io-client";
-const socket = io(process.env.VUE_APP_BACKEND_URL, { autoConnect: false });
-
+const socket = io(process.env.VUE_APP_BACKEND_URL, {
+  transports: ["websocket"],
+  secure: true,
+  autoConnect: false,
+});
 socket.onAny((event, ...args) => {});
 
 export default socket;
